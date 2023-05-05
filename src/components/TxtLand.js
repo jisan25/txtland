27318484fa6c0e9f0df637c8021d3fed1555cee1
Shihabun Mobin Jisan <shihabunjisan@gmail.com>
2023-05-05 22:43:56 +0600

    BTN - clear, upcase, Lcase,Copy, Itlic | 10% Prevw

--- src/components/TxtLand.js
+++ src/components/TxtLand.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 export default function TxtLand(props) {
     const [Text, setText] = useState('');
     const handleChange = (e) => {
-        e.target.value();
+        setText(e.target.value);
     }
     const handleDummy = () => {
         setText('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci corporis suscipit cupiditate asperiores esse totam cumque ipsa repellat, rerum, magnam necessitatibus deleniti quia fugiat laudantium odit accusamus iusto eaque sit, officiis similique. Inventore corporis illo amet architecto laudantium. Accusantium dolores est iure eaque ut facilis alias velit earum, at perspiciatis voluptatem recusandae omnis, iste delectus culpa veniam sapiente veritatis. Aliquid neque saepe consectetur atque vitae cupiditate. Dolores magni enim, autem blanditiis eaque veritatis saepe commodi? Tempora qui doloremque rem id velit sunt delectus corrupti, repellendus quos dolore enim officiis, assumenda, sit at minima ipsa doloribus perferendis! Dicta eveniet similique reiciendis?');
@@ -11,11 +11,27 @@ export default function TxtLand(props) {
 
     const handleTruncate = () => {
         let userInput = prompt("Truncate Value", 50);
-        if (userInput == null || userInput == "") {
+        if (userInput === null || userInput === "") {
             alert("You cancelled the prompt");
         } else {
             setText(Text.substring(0, userInput));
         }
+    }
+
+    const handleClear = () => {
+        setText('');
+    }
+    const handleUpperCase = () => {
+        setText(Text.toUpperCase());
+    }
+    const handleLowerCase = () => {
+        setText(Text.toLowerCase());
+    }
+    const handleCopy = () => {
+        navigator.clipboard.writeText(Text);
+    }
+    const handleItalic = () => {
+        document.getElementById('preview').innerHTML = Text.italics();
     }
     return (
         <>
@@ -30,7 +46,16 @@ export default function TxtLand(props) {
                 <div className='my-3'>
                     <button className='btn btn-warning mx-1' onClick={handleDummy}>Dummy</button>
                     <button className='btn btn-secondary mx-1' onClick={handleTruncate}>Truncate</button>
+                    <button className='btn btn-danger mx-1' onClick={handleClear}>Clear</button>
+                    <button className='btn btn-primary mx-1' onClick={handleUpperCase}>UPPERCASE</button>
+                    <button className='btn btn-info mx-1' onClick={handleLowerCase}>lowercase</button>
+                    <button className='btn btn-success mx-1' onClick={handleCopy}>Copy</button>
+                    <button style={{ backgroundColor: '#D54B01', border: 'none' }} className='btn btn-success mx-1' onClick={handleItalic}>Italic</button>
                 </div>
+            </div>
+            <div className="container">
+                <h2>Preview</h2>
+                <p id='preview'>{Text}</p>
             </div>
         </>
 
